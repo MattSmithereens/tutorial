58409501c3ebf481ec33026c658f08f2b9fba870
Matt Smith <mattsmithereens@gmail.com>
2018-09-18 22:34:31 -0700

    implement redux

---
+++ src/actions/postActions.js
@@ -0,0 +1,23 @@
+import { FETCH_POSTS, NEW_POST } from './types';
+
+export const fetchPosts = () => dispatch => {
+  fetch('http://jsonplaceholder.typicode.com/posts')
+  .then(res => res.json())
+  .then(posts =>
+    dispatch({
+      type:FETCH_POSTS,
+      payload: posts
+    }))
+}
+
+// pre es6 way to do this:
+// export function fetchPosts() {
+//   return function(dispatch) {
+//     fetch('http://jsonplaceholder.typicode.com/posts')
+//     .then(res => res.json())
+//     .then(posts => dispatch({
+//       type: FETCH_POSTS,
+//       payload: posts
+//     }));
+//   }
+// }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
+import PropTypes from 'prop-types';
 
 class Posts extends Component {
-  constructor(props) {
-  super(props);
-  this.state = {
-    posts: []
-  };
-}
-
   componentWillMount() {
-    fetch('http://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(data => this.setState({ posts: data }));
+    this.props.fetchPosts();
   }
 
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -30,4 +24,13 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+Posts.PropTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
+}
+
+const mapStateToProps = state => ({
+  posts: state.posts.items
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
